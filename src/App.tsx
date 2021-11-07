40c22f8dd974610530b2d0480470e4d3ee2f8bbd
basarat <basaratali@gmail.com>
2021-11-08 09:54:08 +1100

    better

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Component } from "solid-js";
+import { Component } from 'solid-js';
 
 const App: Component = () => {
   return (<>
--- src/AppStore.tsx
+++
@@ -1,9 +0,0 @@
-import { createSignal } from "solid-js";
-
-const [elephants, setElephants] = createSignal(0);
-const increasePopulation = () => setElephants(elephants() + 1);
-
-export {
-  elephants,
-  increasePopulation
-};
