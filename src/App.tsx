b29ae276d7736ade93f2d99fb6d99b1ca026e257
basarat <basaratali@gmail.com>
2021-11-10 17:53:39 +1100

    fresh start - recording

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,7 @@
-import { Component, createSignal, For } from 'solid-js';
-import { createCounter, CounterProps, Counter } from './Counter';
+import { Component } from 'solid-js';
 
 const App: Component = () => {
-  const [counters, setCounters] = createSignal<CounterProps[]>([]);
-  const addCounter = () => {
-    setCounters([...counters(), createCounter()]);
-  };
-
   return (<>
-    <button onClick={addCounter}>Add Counter</button>
-    <p>Total Count: {counters().reduce((a, c) => a + c.count(), 0)}</p>
-
-    <For each={counters()}>
-      {counter => <Counter {...counter} />}
-    </For>
   </>);
 };
 
